b77377ea39b26603bd8012f6bbcb7cc682139e25
Shashank <chikoti.shashank1706@gmail.com>
2024-03-09 12:52:16 +0530

    Just made few change's in the python file

--- index.py
+++ index.py
@@ -1,1 +1,6 @@
-print("Hello World")
+print("Welcome, Boss continue your progress")
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+c = a + b
+print("The sum of two numbers that you have entered is:45",c)
